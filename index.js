f9e1a828a3ceb500b1a0df01128bf3ebc48b7f61
RajadorDev <rajadordeveloper@gmail.com>
2025-05-16 18:44:31 -0300

    Remove bot token configuration from botinfo.json and update index.js to eliminate dependency on it

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const { Client, Intents, Events, GatewayIntentBits } = require('discord.js')
 
 bot = new Client({intents: GatewayIntentBits.Guilds})
 
-const { token } = require('./botinfo.json')
-
 bot.once(Events.ClientReady, client => {
 	console.log('Your bot: ' + client.user.tag + ' is Online :)')
 })
