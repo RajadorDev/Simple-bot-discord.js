016f89f463bfdafddbf630362bd06758241de08c
RajadorDev <rajadordeveloper@gmail.com>
2025-05-16 19:40:41 -0300

    Initialize project with package.json, .gitignore, and update index.js for environment variable handling

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const { Client, Intents, Events, GatewayIntentBits } = require('discord.js')
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-bot = new Client({intents: GatewayIntentBits.Guilds})
+bot = new Client({intents: GatewayIntentBits.Guilds});
+
+require('dotenv').config();
+
+const token = process.env.DISCORD_BOT_TOKEN;
 
 bot.once(Events.ClientReady, client => {
 	console.log('Your bot: ' + client.user.tag + ' is Online :)')
-})
+});
 
-bot.login(token)
+bot.login(token);
